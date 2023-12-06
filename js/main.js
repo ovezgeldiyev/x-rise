70480272dfcd4cf7ee50a98f54e408fb2810287e
ovezgeldiyev <ovezgeldiyevagabay2000@gmail.com>
2023-12-06 13:04:52 +0500

    asd

---
+++ js/main.js
@@ -0,0 +1,52 @@
+// menu start
+var menu = document.getElementById("menu");
+var menuBtn = document.getElementById("menuBtn");
+var body = document.body;
+let header = document.getElementById("header");
+menuBtn.onclick = function () {
+  menu.classList.toggle("active");
+  menuBtn.classList.toggle("active");
+  body.classList.toggle("active");
+  header.classList.toggle("active");
+};
+window.onclick = function (event) {
+  if (event.target == menu) {
+    menu.classList.remove("active");
+    menuBtn.classList.remove("active");
+    body.classList.remove("active");
+    header.classList.remove("active");
+  }
+};
+// menu end
+// scroll start
+function scrollFunc() {
+  if (window.pageYOffset >= 60) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+const links = document.querySelectorAll(".links");
+const sections = document.querySelectorAll(".anchor");
+function changeLinkState() {
+  let index = sections.length;
+  while (--index && window.scrollY + 100 < sections[index].offsetTop) {}
+  links.forEach((link) => link.classList.remove("active"));
+  links[index]?.classList.add("active");
+}
+links.forEach((e) => {
+  onLinkClick(e);
+});
+function onLinkClick(linkItem) {
+  linkItem.addEventListener("click", function () {
+    menu.classList.remove("active");
+    menuBtn.classList.remove("active");
+    body.classList.remove("active");
+  });
+}
+window.onscroll = function () {
+  changeLinkState();
+  scrollFunc();
+};
+// scroll end
+
